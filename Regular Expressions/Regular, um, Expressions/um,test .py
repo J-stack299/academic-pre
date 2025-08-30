79968f4276be5ccce7fa324a5d46464e91d89515
J-stack299 <jamieleegordon63@gmail.com>
2025-08-31 00:39:47 +0200

    Add files via upload

---
+++ Regular Expressions/Regular, um, Expressions/um,test .py
@@ -0,0 +1,24 @@
+from um import count 
+
+def main():
+    test_upper_lower_case()
+    test_word_with_um()
+    test_surrounded_by_space()
+
+
+def test_upper_lower_case():
+    assert count('Um, thanks for the album.')
+    assert count('Um, thanks, um...') == 2
+
+
+def test_word_with_um():
+    assert count('yummi') == 0
+
+
+def test_surrounded_by_space():
+    assert count('Hello um world') == 1
+    assert count('um?') == 1
+
+
+if __name__="_main__":
+    main()
